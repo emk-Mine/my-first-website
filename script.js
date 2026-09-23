2de3de4ced180a6ade48a9a76e8035872714f642
emk-Mine <edward.munkanta@cs.unza.zm>
2026-09-23 06:42:43 -0700

    Add product images

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const products = [
         name: "Product One",
         price: 10,
         description: "This is the description of product one.",
-        image: "product1.jpg",
+        image: "images/product1.jpg",
         location: "Lusaka"
     },
 
